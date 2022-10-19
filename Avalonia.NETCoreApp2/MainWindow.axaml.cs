dbcf9c8825d918958829eb0e008cc5c33bcc7afd
@prokoppos <prokoppospisil@gmail.com>
2022-10-19 11:34:56 +0200

    op

---
+++ Avalonia.NETCoreApp2/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Avalonia.NETCoreApp2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
